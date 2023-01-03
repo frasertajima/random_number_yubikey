7f2e0dc54f5c37f61cf9fdf37c59bc9b74865aae
frasertajima <69366820+frasertajima@users.noreply.github.com>
2023-01-03 00:41:30 -0800

    Update random_number_generator_yubikey_version2.py

--- random_number_generator_yubikey_version2.py
+++ random_number_generator_yubikey_version2.py
@@ -1,22 +1,17 @@
 # high quality random number generator using Yubikey hardware
-# only works in Debian at the moment due to formatting command line commands "| tr -dc 0-9 | xargs"
-# if you want letters change '0-9' to '0-z' and dont use int()
+# works in Debian using formatting command line "| tr -dc 0-9 | xargs"
+# if you want letters, change '0-9' to '0-z' and dont use int()
 
 # https://forum.yubico.com/viewtopic07a2.html?p-7040 has the formatting commands to convert bytes to numbers
 # my modification as the original had "tr -dc 0-z" while I changed it to "tr -dc 0-9" because I only wanted numbers
 
-# you can change the length of the random numbers from 1000 to bigger or smaller numbers
+# you can specify the largest random number (otherwise the variation can be large)
 # this generates a list of numbers in the random_numbers_yubikey_generated.csv file
 # use GOOGLE sheets or Libreoffice for handling long numbers (excel truncates them!!)
 
-# you can also modify the output to create a high quality password generator! (see random_password_generator_yubikey.py)
+# you can also modify the output to create a high quality password generator (see random_password_generator_yubikey.py)
 # at the command line gpg-connect-agent "scd random 128" /bye generates random bytes of length 128
 # this program takes that output and formats it into numbers only with '| tr -dc 0-9 | xargs' command in Debian
-
-# still looking for the Windows equivalent commands 
-# and getting yubikey to work with WSL elegantly (there are some hacks but...)
-# saves the generated random numbers into a csv file for convenience
-# this variation asks for inputs on the number of random numbers and the biggest random number (unbound the range is pretty extreme)
 
 # make sure yubikey is linked with your Linux OS when running in a new VM: https://blog.programster.org/yubikey-link-with-gpg
 
